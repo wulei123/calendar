db0ee9477f00f9d057c1c304dacef557710a61ac
wulei123 <1534563365@qq.com>
2016-06-29 23:12:08 +0800

    calendar的重置yeoman版 目前只是手机端  基础样式马上就完成了 数据考完试再说

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
 console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+  $('.addButton').click(function () {
+    if($(this).hasClass('addButton-active')){
+      $(this).removeClass('addButton-active');
+      $('.addReminder').removeClass('addReminder-open');
+      $('.addReminder').addClass('addReminder-close');
+    }else{
+      $(this).addClass('addButton-active');
+      $('.addReminder').removeClass('addReminder-close');
+      $('.addReminder').addClass('addReminder-open');
+    }
+  });
+  
+  
+  
+  
+});
