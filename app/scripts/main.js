91af5fae6b128efd8a5f150a3408bc853d91bdf6
wulei123 <1534563365@qq.com>
2016-06-30 15:35:45 +0800

    日历可以滑动了 但是月份数据还没有写 明天考试 先学习

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,16 +3,79 @@ $(document).ready(function () {
   $('.addButton').click(function () {
     if($(this).hasClass('addButton-active')){
       $(this).removeClass('addButton-active');
-      $('.addReminder').removeClass('addReminder-open');
-      $('.addReminder').addClass('addReminder-close');
+
     }else{
       $(this).addClass('addButton-active');
-      $('.addReminder').removeClass('addReminder-close');
-      $('.addReminder').addClass('addReminder-open');
     }
   });
-  
-  
-  
-  
+
+  $('.calendar').on('touchstart',function (e) {
+    var touch = e.originalEvent.targetTouches[0];
+    var x = touch.clientX - $(this).offset().left;
+    console.log('left'+$(this).offset().left);
+    console.log(x);
+    $(this).on('touchmove',function (e) {
+      var touch = e.originalEvent.targetTouches[0];
+      var tmp1 = touch.clientX - x;
+      console.log('change '+tmp1+' '+$(this).index())
+      $(this).on('touchend',function (e) {
+        if(tmp1>100){
+          last($(this).index());
+        }else if(tmp1<-100){
+          next($(this).index());
+        }
+      });
+
+    })
+  })
+  function last(t){
+    t--;
+    if(t<0)
+      t=0;
+    $('.calendar').css({
+      'transform':'translate(-'+t*111.11111+'%,0)'
+    });
+  }
+  function next(t){
+    t++;
+    if(t>2)
+      t=2;
+    $('.calendar').css({
+      'transform':'translate(-'+t*111.11111+'%,0)'
+    });
+  }
+
+  /*
+
+   document.getElementById("calendar").addEventListener('touchstart',function (e) {
+   var x=e.touches[0].pageX;
+   //console.log(e.touches[0].clientX);
+   document.addEventListener('touchmove',function (e) {
+   //console.log(e.touches[0].clientX);
+   var tmp1 = e.touches[0].pageX-x;
+   if(tmp1>100){
+   $('.calendar').css({
+   'transform':'translate('+1000/9+'%,0)'
+   });
+   }else if(tmp1<-100){
+   $('.calendar').css({
+   'transform':'translate(-'+1000/9+'%,0)'
+   });
+   }else{
+   $('.calendar').css({
+   'transform':'translate(0,0)',
+   });
+   }
+
+   })
+   },false);
+
+
+
+   */
+
+
+
+
+
 });
